01330a695e0eec307a5ce4251454d79da25dba7d
HarjotIris <harjotsingh22nov@gmail.com>
2026-04-06 15:09:46 +0530

    Readying dashboard for deployment

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://harjotiris.github.io"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
